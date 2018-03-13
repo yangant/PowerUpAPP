4a262b0bde68183c88bf26eb116dd0889f2c3784
Sunny <583954049@qq.com>
2018-03-13 21:07:13 +0800

    底部导航栏功能实现
    实现底部导航栏切换界面功能

--- PowerUp/app/src/main/java/com/example/administrator/powerup/MainActivity.java
+++ PowerUp/app/src/main/java/com/example/administrator/powerup/MainActivity.java
@@ -1,44 +1,72 @@
 package com.example.administrator.powerup;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_me);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_task);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_rank);
-                    return true;
-            }
-            return false;
-        }
-    };
+    ViewPager mainActivityViewPager;
+    BottomNavigationView bottomNavView;
+    MainActivityViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        获取到两个控件的对象
+        mainActivityViewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        bottomNavView = (BottomNavigationView) findViewById(R.id.navigation);
+//        为ViewPager设置Adapter
+        adapter = new MainActivityViewPagerAdapter(getSupportFragmentManager());
+//        为Adapter添加Fragment
+        adapter.addFragment(new BlankFragmentOne());
+        adapter.addFragment(new BlankFragmentTwo());
+        mainActivityViewPager.setAdapter(adapter);
+//        为 BottomNavigationView 的菜单项  设置监听事件
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//               获取到菜单项的Id
+                int itemId = item.getItemId();
+//                当第一项被选择时BlankFragmentOne显示，以此类推
+                switch (itemId) {
+                    case R.id.navigation_home:
+                        mainActivityViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.navigation_dashboard:
+                        mainActivityViewPager.setCurrentItem(1);
+                        break;
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+                }
+                // true 会显示这个Item被选中的效果 false 则不会
+                return true;
+            }
+        });
+//        为 ViewPager 设置监听事件
+        mainActivityViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+//                当 ViewPager 滑动后设置BottomNavigationView 选中相应选项
+                bottomNavView.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
-
 }
+
